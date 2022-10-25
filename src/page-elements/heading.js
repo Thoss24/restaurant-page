1d9ebfacdfc3ed8ba35892f2a9e39b0c8f73d205
Thomas Fogarty <t.fogarty24@outlook.com>
2022-10-25 13:35:50 +0100

    added a seperate js file with header elements

---
+++ src/page-elements/heading.js
@@ -0,0 +1,9 @@
+function headerSection() {
+   const headerDiv = document.createElement('div');
+   headerDiv.id = "header";
+
+   const headerTitle = document.createElement('h1');
+   headerDiv.appendChild(headerTitle)
+};
+
+export default headerSection;
