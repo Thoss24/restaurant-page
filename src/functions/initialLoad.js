c3182811d2f657cc6c8202d6c98247c9ddd3def4
Thomas Fogarty <t.fogarty24@outlook.com>
2022-10-25 15:44:06 +0100

    added h1 element to header section

--- src/functions/initialLoad.js
+++ src/functions/initialLoad.js
@@ -3,6 +3,12 @@ import headerSection from "../page-elements/heading";
 const initialLoad = function() {
     const mainContent = document.querySelector('#content');
     const header = headerSection();
+
+    const headerTitle = document.createElement('h1');
+    headerTitle.textContent = "My Restaurant";
+
+    header.appendChild(headerTitle);
+
     mainContent.appendChild(header);
 };
 
