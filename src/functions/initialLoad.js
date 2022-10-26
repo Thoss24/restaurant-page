f7c4ddd9aa8af2864849706e071facca9c2f2f78
Thomas Fogarty <t.fogarty24@outlook.com>
2022-10-26 14:53:37 +0100

    added DOM elements to body section and imported to index.js

--- src/functions/initialLoad.js
+++ src/functions/initialLoad.js
@@ -1,10 +1,13 @@
 import headerSection from "../page-elements/heading";
+import bodySection from "../page-elements/body";
 
 const initialLoad = function() {
     const mainContent = document.querySelector('#content');
     const header = headerSection();
+    const bodyArea = bodySection();
 
     mainContent.appendChild(header);
+    mainContent.appendChild(bodyArea)
 };
 
 export default initialLoad;
--- src/page-elements/body.js
+++ src/page-elements/body.js
@@ -2,6 +2,21 @@ function bodySection() {
     const bodyDiv = document.createElement('div');
     bodyDiv.id = 'bodyArea';
 
+    const mainDiv = document.createElement('div');
+    mainDiv.id = 'mainDiv';
+
+    const mainDivTitleArea = document.createElement('div');
+    mainDivTitleArea.id = 'mainDivTitleArea';
+
+    const mainDivTitle = document.createElement('h2');
+    mainDivTitle.textContent = "Restaurant menu"
+
+    bodyDiv.appendChild(mainDiv);
+    mainDiv.appendChild(mainDivTitleArea);
+    mainDivTitleArea.appendChild(mainDivTitle);
+
     return bodyDiv
 };
+
+export default bodySection;
 
