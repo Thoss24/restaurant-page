62084334f5889cce5e6cc5cf8c3c161182587e74
Thomas Fogarty <t.fogarty24@outlook.com>
2022-10-27 15:41:34 +0100

    added list items to heading

--- src/page-elements/heading.js
+++ src/page-elements/heading.js
@@ -3,9 +3,23 @@ function headerSection() {
    headerDiv.id = "header";
 
    const headerTitle = document.createElement('h1');
-   headerTitle.textContent = "My Restaurant";
+   headerTitle.textContent = "MY RESTAURANT";
+
+   const headerList = document.createElement('ul');
+   headerList.id = "headerList";
+   const headerMenu = document.createElement('li');
+   const headerAbout = document.createElement('li');
+   const headerContact = document.createElement('li');
+   headerMenu.textContent = "MENU";
+   headerAbout.textContent = "ABOUT";
+   headerContact.textContent = "CONTACT";
+   headerList.appendChild(headerMenu);
+   headerList.appendChild(headerAbout);
+   headerList.appendChild(headerContact);
 
    headerDiv.appendChild(headerTitle);
+
+   headerDiv.appendChild(headerList)
 
    return headerDiv;
 };
